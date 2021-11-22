f3b8513b09513703d0b1798b43982a7b98101cd1
andreasturiale <57037312+andreasturiale@users.noreply.github.com>
2021-11-22 09:34:41 +0100

    Update ProfileViewModel.cs

--- Tailspin.SpaceGame.Web/Models/ProfileViewModel.cs
+++ Tailspin.SpaceGame.Web/Models/ProfileViewModel.cs
@@ -4,7 +4,8 @@
     {
         // The player profile.
         public Profile Profile;
+        //comment in the branch-2
         // The player's rank according to the active filter.
-        public string Rank;
+        public string Rank2;
     }
 }
